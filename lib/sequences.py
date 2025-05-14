2a067b8a37823b9371d36eb05ed244c866bec7fc
O-liverr <oliekoech@gmail.com>
2025-05-15 01:04:32 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length <= 0:
+        print([])
+        return
+    for i in range(length):
+        if i == 0:
+            fib_list.append(0)
+        elif i == 1:
+            fib_list.append(1)
+        else:
+            fib_list.append(fib_list[i-1] + fib_list[i-2])
+    print(fib_list)
